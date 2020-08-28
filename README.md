35b7238d38348deaabab7855505bf9cf280fdc28
Johannes Flake <johannes.flake@gmail.com>
2020-08-28 10:06:58 +0200

    specify port; readme

--- README.md
+++ README.md
@@ -19,5 +19,13 @@ git pull
 ```
 should update the files and start a Jupyter Notebook Server. Click on the links and open the example.
 
+## Specify the port
+
+If you want to run the Notebook Server on a specific port, say, because you want to use a computing server via `ssh`, the last command can be modified like this:
+```
+./run.sh 8788
+```
+
+
 
 
--- run.sh
+++ run.sh
@@ -1,2 +1,5 @@
+port=8888
+[ -z "$1" ] || port="$1"
+
 docker pull johannesflake/polybases
-docker run -v $(pwd):/home/oscar/local -w /home/oscar/local -p 8888:8888 -it johannesflake/polybases jupyter-notebook --ip 0.0.0.0 --port 8888 --no-browser
+docker run -v $(pwd):/home/oscar/local -w /home/oscar/local -p $port:$port -it johannesflake/polybases jupyter-notebook --ip 0.0.0.0 --port $port --no-browser
