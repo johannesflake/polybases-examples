cd5bf53e00b54555d0b5aaf75a15f2f4077b8d64
johannesflake <flake@art.rwth-aachen.de>
2022-01-12 18:28:06 +0100

    updates, fix notebook after refactoring in package

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 port=8888
 [ -z "$1" ] || port="$1"
 
-docker pull johannesflake/oscar
-docker run -v $(pwd):/home/oscar/local -w /home/oscar/local -p $port:$port -it johannesflake/oscar jupyter-lab --ip 0.0.0.0 --port $port --no-browser
+docker pull johannesflake/oscar:LTS
+docker run -v $(pwd):/home/oscar/local -w /home/oscar/local -p $port:$port -it johannesflake/oscar:LTS jupyter-lab --ip 0.0.0.0 --port $port --no-browser
